82f9d3a9aed81ebadacd52d596dc4fce9e699837
Judhveer <judhveer@proton.me>
2025-10-22 15:45:42 +0530

    fix: cron jobs syncData

--- backend/src/jobs/attendance/syncAllData.js
+++ backend/src/jobs/attendance/syncAllData.js
@@ -1,39 +1,73 @@
-import axios from 'axios';
-import cron from 'node-cron';
+import axios from "axios";
+import cron from "node-cron";
 const BASE_URL = process.env.BASE_URL;
-import attendanceController from '../../controllers/attendanceController/attendanceController.js';
+import attendanceController from "../../controllers/attendanceController/attendanceController.js";
 let { isSyncing } = attendanceController;
 
-
-
-
-// schedule cron job to run every Sunday at 4 AM
 export function AttendanceSyncAll() {
-    
-    cron.schedule('0 4 * * 0', async () => {
+  // syncAll
+  // mon-sat 11:59 pm
+  cron.schedule("59 23 * * 1-6", async () => {
+    try {
+      console.log("Running Attendance SyncAll");
+      const res = await axios.get(`${BASE_URL}/api/attendance/syncAll`);
+      console.log("Attendance syncAll result:", res.data);
+    } catch (error) {
+      console.error(
+        "Attendance sync cron failed:",
+        error.response?.data || error.message
+      );
+    }
+  });
 
-        try {
-            console.log('Running attendance sync cron job (office hours)...');
-            const res = await axios.get(`${BASE_URL}/api/attendance/syncAll`);
-            console.log('Attendance syncAll result:', res.data);
-        } catch (error) {
-            console.error('Attendance sync cron failed:', error.response?.data || error.message);
-        }
-    });
+  // schedule cron job to run every Sunday at 4 AM
+  cron.schedule("0 4 * * 0", async () => {
+    try {
+      console.log("Running Attendance SyncAll");
+      const res = await axios.get(`${BASE_URL}/api/attendance/syncAll`);
+      console.log("Attendance syncAll result:", res.data);
+    } catch (error) {
+      console.error(
+        "Attendance sync cron failed:",
+        error.response?.data || error.message
+      );
+    }
+  });
 
+  // sync
+  // everyday at 12 noon
+  cron.schedule("0 12 * * *", async () => {
+    if (isSyncing) {
+      console.log("Sync already running (cron).");
+      return;
+    }
+    try {
+      console.log("Running attendance sync cron job (office hours)...");
+      const res = await axios.get(`${BASE_URL}/api/attendance/sync`);
+      console.log("Attendance sync result:", res.data);
+    } catch (error) {
+      console.error(
+        "Attendance sync cron failed:",
+        error.response?.data || error.response?.message || error.message
+      );
+    }
+  });
 
-    cron.schedule('0 22 * * *', async () => {
-        if (isSyncing) {
-            console.log('Sync already running (cron).');
-            return;
-        }
-        try {
-            console.log('Running attendance sync cron job (office hours)...');
-            const res = await axios.get(`${BASE_URL}/api/attendance/sync`);
-            console.log('Attendance sync result:', res.data);
-        } catch (error) {
-            console.error('Attendance sync cron failed:', error.response?.data || error.response?.message || error.message);
-        }
-    });
-
+  // everyday at 10:00 PM
+  cron.schedule("0 22 * * *", async () => {
+    if (isSyncing) {
+      console.log("Sync already running (cron).");
+      return;
+    }
+    try {
+      console.log("Running attendance sync cron job (office hours)...");
+      const res = await axios.get(`${BASE_URL}/api/attendance/sync`);
+      console.log("Attendance sync result:", res.data);
+    } catch (error) {
+      console.error(
+        "Attendance sync cron failed:",
+        error.response?.data || error.response?.message || error.message
+      );
+    }
+  });
 }
