31c1541051630b3d094cffd468c675c86d460b7d
Judhveer <judhveer@proton.me>
2026-04-07 17:58:20 +0530

    feat: outBound Dashboard

---
+++ frontend/src/pages/jobFms/OutBoundDashboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import OutboundOrders from "../../components/jobFms/OutboundOrders.jsx";
+export default function CrmDashboard() {
+  return <div>
+    <OutboundOrders />
+  </div>;
+}
