ed3839d0b0c1d19185a018d15c257db5f7ddfe31
Judhveer <judhveer@proton.me>
2026-04-10 15:47:18 +0530

    fix: JobAssignment new cancelled enum is added in status field

--- backend/src/models/jobFmsModels/JobAssignment.model.js
+++ backend/src/models/jobFmsModels/JobAssignment.model.js
@@ -53,6 +53,7 @@ export default (sequelize) => {
           "assigned",
           "in_progress",
           "completed",
+          "cancelled",
         ),
         defaultValue: "assigned",
       },
