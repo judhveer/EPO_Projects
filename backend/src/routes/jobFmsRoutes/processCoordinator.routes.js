09ba9fb1a22e474faf0231902a43962e6cc5997b
Judhveer <judhveer@proton.me>
2026-05-08 15:18:33 +0530

    feat: new route for set-estimated-time by process coordinator.

--- backend/src/routes/jobFmsRoutes/processCoordinator.routes.js
+++ backend/src/routes/jobFmsRoutes/processCoordinator.routes.js
@@ -2,13 +2,15 @@ import express from "express";
 import {
 getAllJobsForProcessCoordinator,
 assignDesigner,
-getDesignerStatus
+getDesignerStatus, 
+coordinatorSetEstimatedTime
 } from "../../controllers/jobFmsController/processCoordinator.controller.js";
 
 const router = express.Router();
 
 router.get("/jobs", getAllJobsForProcessCoordinator);
 router.patch("/:job_no/assign", assignDesigner);
+router.patch("/:job_no/set-estimated-time", coordinatorSetEstimatedTime);
 router.get("/designers/status", getDesignerStatus);
 
 export default router;
